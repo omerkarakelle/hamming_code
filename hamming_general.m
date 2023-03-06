793225d526e10a23ed81ec3ff61278de63573364
Ömer Lütfü Karakelle <71152385+omerkarakelle@users.noreply.github.com>
2023-03-06 13:46:48 +0300

    Update hamming_general.m

--- hamming_general.m
+++ hamming_general.m
@@ -2,7 +2,7 @@ data = [1 0 0 1] %input
 n = 7 %input
 k = 4 %input
 
-m = log2(n + 1)
+m = log2(n + 1);
 
 if not(k == length(data))
     fprintf("Error! Length of the input and the value of k does not match.\n")
@@ -13,43 +13,43 @@ if not(k == power(2, m) - m - 1)
     return
 end
 
-A = ft_matrix(n,k)
+A = ft_matrix(n,k); 
 
-genmat = [eye(k) A]
-par_check = [A.' eye(n - k)]
-r = [eye(k) zeros(k, n - k)]
+genmat = [eye(k) A];
+par_check = [A.' eye(n - k)];
+r = [eye(k) zeros(k, n - k)];
 
-codeword = mod(data * genmat, 2)
+codeword = mod(data * genmat, 2);
 
 function y = is_power_of_2(x)
-    y = 0
-    i = 0
+    y = 0;
+    i = 0;
     while pow2(i) <= x 
         if pow2(i) == x
-            y = 1
+            y = 1;
             return
         end
-        i = i + 1
+        i = i + 1;
     end
 end
 
-function A = ft_matrix(n, k)
-    A = zeros(k, n - k)
-    t = 1
+function A = ft_matrix(n, k) %this function generates a generator matrix
+    A = zeros(k, n - k);
+    t = 1;
     for i = 1:(n - k)*k
         while is_power_of_2(t) == 1
-            t = t + 1
+            t = t + 1;
         end
-        x = de2bi(t)
+        x = de2bi(t);
         if fix((i-1) / k) + 1 > length(x)
-            A(i) = 0
+            A(i) = 0;
         else
-            A(i) = x(fix((i-1) / k) + 1)
+            A(i) = x(fix((i-1) / k) + 1);
         end
         if t == n
-            t = 1
+            t = 1;
         else
-            t = t + 1
+            t = t + 1;
         end
     end
 end
